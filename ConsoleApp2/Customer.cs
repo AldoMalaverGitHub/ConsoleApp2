f48ad32bfa5b94aa7adc9869735deb1be69c6dfa
aldomalaverGitHub <almalaver@gmail.com>
2019-03-31 05:51:07 +0000

    queue example

---
+++ ConsoleApp2/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2.Entities;
+
+namespace ConsoleApp2
+{
+    class Customer
+    {
+        static void CreateCustomer(CloudTable cloudTable, CustomerUS customerUS)
+        {
+            TableOperation insertOperation = TableOperation.Insert(customerUS);
+
+            cloudTable.Execute(insertOperation);
+        }
+
+        static void GetCustomer(CloudTable cloudTable, string partitionKey, string rowKey)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerUS>(partitionKey, rowKey);
+            var result = cloudTable.Execute(retrieveOperation);
+            Console.WriteLine(((CustomerUS)result.Result).Name);
+        }
+
+        static CustomerUS GetOneCustomer(CloudTable cloudTable, string partitionKey, string rowKey)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerUS>(partitionKey, rowKey);
+            var result = cloudTable.Execute(retrieveOperation);
+            return (CustomerUS)result.Result;
+        }
+
+        static void GetAllCustomers(CloudTable cloudTable)
+        {
+            TableQuery<CustomerUS> query = new TableQuery<CustomerUS>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "US"));
+            foreach (CustomerUS customerUs in cloudTable.ExecuteQuery(query))
+            {
+                Console.WriteLine(customerUs.Name);
+            }
+        }
+
+        static void UpdateCustomer(CloudTable cloudTable, CustomerUS customerUS)
+        {
+            TableOperation updateOperation = TableOperation.Replace(customerUS);
+            cloudTable.Execute(updateOperation);
+        }
+
+        static void DeleteCustomer(CloudTable cloudTable, CustomerUS customerUS)
+        {
+            TableOperation deleteOperation = TableOperation.Delete(customerUS);
+            cloudTable.Execute(deleteOperation);
+        }
+
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApp2.Entities;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
@@ -16,77 +17,53 @@ namespace ConsoleApp2
         {
             CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnection"));
 
-            CloudTableClient cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
+            /*   CloudTableClient cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
 
-            CloudTable cloudTable = cloudTableClient.GetTableReference("customers");
+               CloudTable cloudTable = cloudTableClient.GetTableReference("customers");
 
-            cloudTable.CreateIfNotExists();
+               cloudTable.CreateIfNotExists();
 
-            TableBatchOperation tableBatchOperation = new TableBatchOperation();
+               TableBatchOperation tableBatchOperation = new TableBatchOperation();
 
-            var customer1 = new CustomerUS("Aldo", "almalaver@hotmail.com");
-            var customer2 = new CustomerUS("Junior", "pxamalav@cibertec.edu.pe");
-            var customer3 = new CustomerUS("Paty", "npatrimc@hotmail.com");
+               var customer1 = new CustomerUS("Aldo", "almalaver@hotmail.com");
+               var customer2 = new CustomerUS("Junior", "pxamalav@cibertec.edu.pe");
+               var customer3 = new CustomerUS("Paty", "npatrimc@hotmail.com");
 
-            tableBatchOperation.Insert(customer1);
-            tableBatchOperation.Insert(customer2);
-            tableBatchOperation.Insert(customer3);
+               tableBatchOperation.Insert(customer1);
+               tableBatchOperation.Insert(customer2);
+               tableBatchOperation.Insert(customer3);
 
-            cloudTable.ExecuteBatch(tableBatchOperation);
-            GetAllCustomers(cloudTable);
-            //CreateCustomer(cloudTable, new CustomerUS("Junior", "almalaver1@gmail.com"));
-            //GetCustomer(cloudTable, "US", "almalaver@gmail.com");
-            //GetAllCustomers(cloudTable);
+               cloudTable.ExecuteBatch(tableBatchOperation);
+               GetAllCustomers(cloudTable);
+               //CreateCustomer(cloudTable, new CustomerUS("Junior", "almalaver1@gmail.com"));
+               //GetCustomer(cloudTable, "US", "almalaver@gmail.com");
+               //GetAllCustomers(cloudTable);
 
-            //var aldoCustomer = GetOneCustomer(cloudTable, "US", "almalaver@gmail.com");
-            // aldoCustomer.Name = "Aldo Junior";
-            // UpdateCustomer(cloudTable, aldoCustomer);
-            //DeleteCustomer(cloudTable, aldoCustomer);
-            //GetAllCustomers(cloudTable);
+               //var aldoCustomer = GetOneCustomer(cloudTable, "US", "almalaver@gmail.com");
+               // aldoCustomer.Name = "Aldo Junior";
+               // UpdateCustomer(cloudTable, aldoCustomer);
+               //DeleteCustomer(cloudTable, aldoCustomer);
+               //GetAllCustomers(cloudTable);
+
+       */
+            CloudQueueClient cloudQueueClient = cloudStorageAccount.CreateCloudQueueClient();
+            CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("tasks");
+            cloudQueue.CreateIfNotExists();
+
+            //CloudQueueMessage cloudQueueMessage = new CloudQueueMessage("HOla papi");
+            //var time = new TimeSpan(24, 0, 0);
+            //cloudQueue.AddMessage(cloudQueueMessage, time, null, null);
+            
+            CloudQueueMessage cloudQueueMessage = cloudQueue.GetMessage();
+            Console.WriteLine(cloudQueueMessage.AsString);
+            cloudQueue.DeleteMessage(cloudQueueMessage);
+
+            //CloudQueueMessage cloudQueueMessage = cloudQueue.PeekMessage();
+           // Console.WriteLine(cloudQueueMessage.AsString);
+           
             Console.ReadKey();
         }
 
-        static void CreateCustomer(CloudTable cloudTable , CustomerUS customerUS)
-        {
-            TableOperation insertOperation = TableOperation.Insert(customerUS);
-
-            cloudTable.Execute(insertOperation);
-        }
-
-        static void GetCustomer(CloudTable cloudTable, string partitionKey, string rowKey)
-        {
-            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerUS>(partitionKey, rowKey);
-            var result = cloudTable.Execute(retrieveOperation);
-            Console.WriteLine(((CustomerUS)result.Result).Name);
-        }
-
-        static CustomerUS GetOneCustomer(CloudTable cloudTable, string partitionKey, string rowKey)
-        {
-            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerUS>(partitionKey, rowKey);
-            var result = cloudTable.Execute(retrieveOperation);
-            return (CustomerUS)result.Result;
-        }
-
-        static void GetAllCustomers(CloudTable cloudTable)
-        {
-            TableQuery<CustomerUS> query = new TableQuery<CustomerUS>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "US"));
-            foreach(CustomerUS customerUs in cloudTable.ExecuteQuery(query))
-            {
-                Console.WriteLine(customerUs.Name);
-            }
-        }
-
-        static void UpdateCustomer(CloudTable cloudTable, CustomerUS customerUS)
-        {
-            TableOperation updateOperation = TableOperation.Replace(customerUS);
-            cloudTable.Execute(updateOperation);
-        }
-
-        static void DeleteCustomer(CloudTable cloudTable, CustomerUS customerUS)
-        {
-            TableOperation deleteOperation = TableOperation.Delete(customerUS);
-            cloudTable.Execute(deleteOperation);
-        }
-
+        
     }
 }
